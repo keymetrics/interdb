762a8dd31453eb43b3d6eaced4dae498de313bce
ItsJimi <jimi.maiquez@gmail.com>
2018-01-29 18:36:19 +0100

    New constant

--- constants.js
+++ constants.js
@@ -1,5 +1,6 @@
 
 module.exports = {
   UPDATE_DATABASE: 'interdb:command:updateDb',
-  SYNC_DATABASE: 'interdb:command:sync'
+  SYNC_DATABASE: 'interdb:command:sync',
+  LASTUPDATE_DATABASE: 'interdb:command:lastUpdate'
 }
